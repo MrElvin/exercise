66cd6f68dbf3ebf41751b52d4283e28ff19b0147
MrElvin <935448078@qq.com>
2017-05-28 21:12:42 +0800

    fix src agian last

--- audioPlayer/data.js
+++ audioPlayer/data.js
@@ -4,87 +4,87 @@ let musicArray = [
         'author': 'Alan Walker',
         'name': 'Faded',
         'image': '/exercise/audioPlayer/images/alan-walker.jpg',
-        'song': '/song/Faded.mp3',
+        'song': '/exercise/audioPlayer/song/Faded.mp3',
         'isLove': false
     },
     {
         'id': 1,
         'author': '赵雷',
         'name': '成都',
-        'image': 'audioPlayer/images/zhaolei.jpg',
-        'song': 'audioPlayer/song/成都.mp3',
+        'image': '/exercise/audioPlayer/images/zhaolei.jpg',
+        'song': '/exercise/audioPlayer/song/成都.mp3',
         'isLove': false
     },
     {
         'id': 2,
         'author': '李玉刚',
         'name': '清明上河图',
-        'image': 'audioPlayer/images/liyugang.jpg',
-        'song': 'audioPlayer/song/清明上河图.mp3',
+        'image': '/exercise/audioPlayer/images/liyugang.jpg',
+        'song': '/exercise/audioPlayer/song/清明上河图.mp3',
         'isLove': false
     },
     {
         'id': 3,
         'author': '陈一发儿',
         'name': '童话镇',
-        'image': 'audioPlayer/images/tonghuazhen.png',
-        'song': 'audioPlayer/song/tonghuazhen.mp3',
+        'image': '/exercise/audioPlayer/images/tonghuazhen.png',
+        'song': '/exercise/audioPlayer/song/tonghuazhen.mp3',
         'isLove': false
     },
     {
         'id': 4,
         'author': '冯提莫',
         'name': '凉凉',
-        'image': 'audioPlayer/images/liangliang.jpg',
-        'song': 'audioPlayer/song/liangliang.mp3',
+        'image': '/exercise/audioPlayer/images/liangliang.jpg',
+        'song': '/exercise/audioPlayer/song/liangliang.mp3',
         'isLove': false
     },
     {
         'id': 5,
         'author': 'Fefe Dobson',
         'name': 'Stuttering',
-        'image': 'audioPlayer/images/Stuttering.jpg',
-        'song': 'audioPlayer/song/Stuttering.mp3',
+        'image': '/exercise/audioPlayer/images/Stuttering.jpg',
+        'song': '/exercise/audioPlayer/song/Stuttering.mp3',
         'isLove': false
     },
     {
         'id': 6,
         'author': '陈一发儿',
         'name': '阿婆说',
-        'image': 'audioPlayer/images/aposhuo.png',
-        'song': 'audioPlayer/song/aposhuo.mp3',
+        'image': '/exercise/audioPlayer/images/aposhuo.png',
+        'song': '/exercise/audioPlayer/song/aposhuo.mp3',
         'isLove': false
     },
     {
         'id': 7,
         'author': '李玟',
         'name': '想你的三百六十五天',
-        'image': 'audioPlayer/images/liwen.jpg',
-        'song': 'audioPlayer/song/liwen.mp3',
+        'image': '/exercise/audioPlayer/images/liwen.jpg',
+        'song': '/exercise/audioPlayer/song/liwen.mp3',
         'isLove': false
     },
     {
         'id': 8,
         'author': '茅野愛衣',
         'name': '君がくれたもの',
-        'image': 'audioPlayer/images/weiwenhuaming.jpg',
-        'song': 'audioPlayer/song/weiwenhuaming.mp3',
+        'image': '/exercise/audioPlayer/images/weiwenhuaming.jpg',
+        'song': '/exercise/audioPlayer/song/weiwenhuaming.mp3',
         'isLove': false
     },
     {
         'id': 9,
         'author': 'Goose house',
         'name': '光るなら',
-        'image': 'audioPlayer/images/siyue.jpg',
-        'song': 'audioPlayer/song/siyue.mp3',
+        'image': '/exercise/audioPlayer/images/siyue.jpg',
+        'song': '/exercise/audioPlayer/song/siyue.mp3',
         'isLove': false
     },
     {
         'id': 10,
         'author': 'Vyacheslav Shuvalov',
         'name': 'My Heart Will Go On',
-        'image': 'audioPlayer/images/taitannikehao.jpg',
-        'song': 'audioPlayer/song/taitannikehao.mp3',
+        'image': '/exercise/audioPlayer/images/taitannikehao.jpg',
+        'song': '/exercise/audioPlayer/song/taitannikehao.mp3',
         'isLove': false
     },
 ];
