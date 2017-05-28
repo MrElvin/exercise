4d7d9cd05b3b380132d086f82308c7f01db74b18
MrElvin <935448078@qq.com>
2017-05-28 20:55:36 +0800

    fix src again

--- audioPlayer/data.js
+++ audioPlayer/data.js
@@ -3,24 +3,24 @@ let musicArray = [
         'id': 0,
         'author': 'Alan Walker',
         'name': 'Faded',
-        'image': 'images/alan-walker.jpg',
-        'song': 'song/Faded.mp3',
+        'image': './images/alan-walker.jpg',
+        'song': './song/Faded.mp3',
         'isLove': false
     },
     {
         'id': 1,
         'author': '赵雷',
         'name': '成都',
-        'image': 'images/zhaolei.jpg',
-        'song': 'song/成都.mp3',
+        'image': './images/zhaolei.jpg',
+        'song': './song/成都.mp3',
         'isLove': false
     },
     {
         'id': 2,
         'author': '李玉刚',
         'name': '清明上河图',
-        'image': 'images/liyugang.jpg',
-        'song': 'song/清明上河图.mp3',
+        'image': './images/liyugang.jpg',
+        'song': './song/清明上河图.mp3',
         'isLove': false
     },
     {
@@ -35,56 +35,56 @@ let musicArray = [
         'id': 4,
         'author': '冯提莫',
         'name': '凉凉',
-        'image': 'images/liangliang.jpg',
-        'song': 'song/liangliang.mp3',
+        'image': './images/liangliang.jpg',
+        'song': './song/liangliang.mp3',
         'isLove': false
     },
     {
         'id': 5,
         'author': 'Fefe Dobson',
         'name': 'Stuttering',
-        'image': 'images/Stuttering.jpg',
-        'song': 'song/Stuttering.mp3',
+        'image': './images/Stuttering.jpg',
+        'song': './song/Stuttering.mp3',
         'isLove': false
     },
     {
         'id': 6,
         'author': '陈一发儿',
         'name': '阿婆说',
-        'image': 'images/aposhuo.png',
-        'song': 'song/aposhuo.mp3',
+        'image': './images/aposhuo.png',
+        'song': './song/aposhuo.mp3',
         'isLove': false
     },
     {
         'id': 7,
         'author': '李玟',
         'name': '想你的三百六十五天',
-        'image': 'images/liwen.jpg',
-        'song': 'song/liwen.mp3',
+        'image': './images/liwen.jpg',
+        'song': './song/liwen.mp3',
         'isLove': false
     },
     {
         'id': 8,
         'author': '茅野愛衣',
         'name': '君がくれたもの',
-        'image': 'images/weiwenhuaming.jpg',
-        'song': 'song/weiwenhuaming.mp3',
+        'image': './images/weiwenhuaming.jpg',
+        'song': './song/weiwenhuaming.mp3',
         'isLove': false
     },
     {
         'id': 9,
         'author': 'Goose house',
         'name': '光るなら',
-        'image': 'images/siyue.jpg',
-        'song': 'song/siyue.mp3',
+        'image': './images/siyue.jpg',
+        'song': './song/siyue.mp3',
         'isLove': false
     },
     {
         'id': 10,
         'author': 'Vyacheslav Shuvalov',
         'name': 'My Heart Will Go On',
-        'image': 'images/taitannikehao.jpg',
-        'song': 'song/taitannikehao.mp3',
+        'image': './images/taitannikehao.jpg',
+        'song': './song/taitannikehao.mp3',
         'isLove': false
     },
 ];
